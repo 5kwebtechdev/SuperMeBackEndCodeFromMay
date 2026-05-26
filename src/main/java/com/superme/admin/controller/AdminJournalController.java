e8bfffcdc0fd6575231be4f3ed5b04c91c3ced8e
Mohit Kumar <mohitj5kwt@gmail.com>
2026-05-26 16:54:01 +0530

    Temporary 7

--- src/main/java/com/superme/admin/controller/AdminJournalController.java
+++ src/main/java/com/superme/admin/controller/AdminJournalController.java
@@ -70,7 +70,7 @@ public class AdminJournalController {
     } else if (isActive != null) {
       criteria.setIsActive(isActive);
     }
-// temp added to prevent teh empty commit
+// temp added to prevent teh empty commitsdsds
     // Delegate all filtering to the service (search + isActive + ranges + engagement + userType)
     AdminJournalOverviewResponse filtered = adminJournalViewService.getFilteredUserJournalViews(criteria);
     List<AdminJournalViewDTO> filteredUsers = filtered.getUsers();
