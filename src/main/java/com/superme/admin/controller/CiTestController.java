61ad9ed7b247acadbde1f2977a7a46855b4ff065
Mohit Kumar <mohitj5kwt@gmail.com>
2026-05-26 18:10:47 +0530

    temp commit

--- src/main/java/com/superme/admin/controller/CiTestController.java
+++ src/main/java/com/superme/admin/controller/CiTestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.Map;
-
+// temp commit
 @RestController
 @RequestMapping("/admin")
 public class CiTestController {
