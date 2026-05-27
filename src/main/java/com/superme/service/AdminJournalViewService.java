61006ace9d5a014b9cbdb41b947e4e3949c1bc5b
Mohit Kumar <mohitj5kwt@gmail.com>
2026-05-27 11:46:06 +0530

    after notes in academy module correct sorting applied 3

--- src/main/java/com/superme/service/AdminJournalViewService.java
+++ src/main/java/com/superme/service/AdminJournalViewService.java
@@ -4,9 +4,12 @@ import com.superme.dto.AdminJournalViewDTO;
 import com.superme.dto.AdminJournalViewDTO.JournalStatistics;
 import com.superme.dto.AdminJournalViewDTO.JournalFilterCriteria;
 import com.superme.dto.AdminJournalOverviewResponse;
-import com.superme.model.Us;
+import com.superme.model.User;
 import com.superme.model.JournalEntry;
 import com.superme.repository.UserRepository;
+
+
+
 import com.superme.repository.JournalEntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
